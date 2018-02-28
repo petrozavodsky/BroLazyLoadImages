cb0eaa048fd51feb853878f45840c5bcb2d91923
petrozvodsky <petrozavodsky@gmail.com>
2018-02-28 12:03:19 +0300

    Ref #5

--- BroLazyLoadImages.php
+++ BroLazyLoadImages.php
@@ -32,10 +32,10 @@ class BroLazyLoadImages extends Wrap {
 	function __construct() {
 		self::$textdomine = $this->setTextdomain();
 
-		if ( current_user_can( 'edit_published_posts' )  ) {
+//		if ( current_user_can( 'edit_published_posts' )  ) {
 			new AddImageSize();
 			new Router();
-		}
+//		}
 
 	}
 
