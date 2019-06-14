a31eb13bc4b5cd5ef48b22ba1c9a7a67133e15c5
petrozavodsky <petrozavodsky@gmail.com>
2019-06-14 16:39:04 +0300

    fix

--- src/BroLazyLoadImages/Classes/Reformer.php
+++ src/BroLazyLoadImages/Classes/Reformer.php
@@ -2,14 +2,16 @@
 
 namespace BroLazyLoadImages\Classes;
 
-class Reformer extends HtmlParser
+class Reformer
 {
 
     private $exclude = [];
 
     private $size;
 
-    public $embed_images = true;
+    public $embedImages = true;
+
+    private $image;
 
     public function __construct($exclude)
     {
@@ -18,81 +20,20 @@ class Reformer extends HtmlParser
 
         $this->exclude = $exclude;
 
-        add_filter('post_thumbnail_html', [$this, 'image_html'], 10, 3);
+        $this->image = new ProgressiveImage();
+
+        add_filter('post_thumbnail_html', [$this, 'imageHtml'], 10, 3);
     }
 
-    public function image_html($html, $post_id, $post_thumbnail_id)
+    public function imageHtml($html, $post_id, $post_thumbnail_id)
     {
         if (!in_array(intval($post_id), $this->exclude)) {
 
-            $preview = $this->insert_image_src($post_thumbnail_id);
-
-            if ($this->embed_images) {
-                $preview = $this->insert_base64_encoded_image_src($post_thumbnail_id);
-            }
-
-            $attributes = $this->getAttributes($html);
-
-            $attributesJson = json_encode($attributes);
-            $attributesBase64 = base64_encode($attributesJson);
-
-            $html = $this->removeAttribute('src', $html);
-            $html = $this->removeAttribute('srcset', $html);
-            $html = $this->removeAttribute('sizes', $html);
-
-            $image = str_replace(
-                ['wp-post-image', "<img"],
-                ['wp-post-image preview', "<img style='max-height: {$attributes['height']}px;' src='{$preview}' "],
-                $html
-            );
-
-            $o = '';
-            $o .= "<div data-attributes='{$attributesBase64}' class='primary progressive replace'>";
-            $o .= $image;
-            $o .= "</div>";
-
-            return $o;
+            return $this->image->html($html, $post_thumbnail_id);
         }
 
         return $html;
 
-    }
-
-
-    public function insert_image_src($image_id)
-    {
-        $preview = wp_get_attachment_image_src($image_id, $this->size);
-
-        return array_shift($preview);
-    }
-
-    public function insert_base64_encoded_image_src($image_id)
-    {
-
-        $data = wp_get_attachment_metadata($image_id);
-        $mime = get_post_mime_type($image_id);
-
-        if (array_key_exists($this->size, $data['sizes'])) {
-            $file_name = $data['sizes'][$this->size]['file'];
-        } else if (0 < count($data['sizes'])) {
-            $first_size = array_shift($data['sizes']);
-            $file_name = $first_size['file'];
-        } else {
-            $file_name = basename($data["file"]);
-        }
-
-        $dirname = dirname($data['file']);
-        $upload_dir = wp_get_upload_dir();
-        $file = $upload_dir['basedir'] . '/' . $dirname . '/' . $file_name;
-
-        if (file_exists($file)) {
-            $imageData = base64_encode(file_get_contents($file));
-            $imageSrc = "data:{$mime};base64,{$imageData}";
-        } else {
-            $imageSrc = $this->insert_image_src($image_id);
-        }
-
-        return $imageSrc;
     }
 
 
